82135595d2478b7baac338d216acca2ea9d07db1
Akshat Sipany <borncancer21@gmail.com>
2024-08-17 16:18:27 +0530

    fixing issue of 1st quote generated

--- src/script.js
+++ src/script.js
@@ -9,7 +9,13 @@ buttons.forEach((button) => {
     fetchQuotes();
   });
 });
-
+function getQuote() {
+    fetchQuotes();
+  }
+  function setupGetQuote() {
+    let btn = document.getElementById("get-quote-btn");
+    btn.addEventListener("click", getQuote);
+  }
 async function fetchQuotes() {
   let res = await fetch(
     "https://api.api-ninjas.com/v1/quotes?category=" + category,
@@ -22,13 +28,7 @@ async function fetchQuotes() {
   displayQuote(data[0].quote, data[0].author);
 }
 // fetchQuotes();
-function getQuote() {
-  fetchQuotes();
-}
-function setupGetQuote() {
-  let btn = document.getElementById("get-quote-btn");
-  btn.addEventListener("click", getQuote);
-}
+
 function displayQuote(quote,author) {
     let quoteTextContainer = document.getElementById('quote');
     quoteTextContainer.innerHTML = ('<h2> "' + quote + '"</h2>');
@@ -36,6 +36,4 @@ function displayQuote(quote,author) {
     let quoteAuthorContainer = document.getElementById('author');
     quoteAuthorContainer.innerHTML = ('<h6>' + "-"+ author + '</h6>');
     // quoteAuthor.innerText = "- " + author;
-
-
 }
