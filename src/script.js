53a39bfd3cbc67312a78f6e62e48a5d54d50b702
Akshat Sipany <borncancer21@gmail.com>
2024-08-18 10:38:50 +0530

    retrieved earlier version due to error in die button

--- src/script.js
+++ src/script.js
@@ -13,12 +13,12 @@ buttons.forEach((button) => {
   });
 });
 function getQuote() {
-  fetchQuotes();
-}
-function setupGetQuote() {
-  let btn = document.getElementById("get-quote-btn");
-  btn.addEventListener("click", getQuote);
-}
+    fetchQuotes();
+  }
+  function setupGetQuote() {
+    let btn = document.getElementById("get-quote-btn");
+    btn.addEventListener("click", getQuote);
+  }
 async function fetchQuotes() {
   let res = await fetch(
     "https://api.api-ninjas.com/v1/quotes?category=" + category,
@@ -32,17 +32,11 @@ async function fetchQuotes() {
 }
 // fetchQuotes();
 
-function displayQuote(quote, author) {
-  let quoteTextContainer = document.getElementById("quote");
-  quoteTextContainer.innerHTML = '<h2> "' + quote + '"</h2>';
-  // quoteText.innerText = quote;
-  let quoteAuthorContainer = document.getElementById("author");
-  quoteAuthorContainer.innerHTML = "<h6>" + "- " + author + "</h6>";
-  // quoteAuthor.innerText = "- " + author;
+function displayQuote(quote,author) {
+    let quoteTextContainer = document.getElementById('quote');
+    quoteTextContainer.innerHTML = ('<h2> "' + quote + '"</h2>');
+    // quoteText.innerText = quote;
+    let quoteAuthorContainer = document.getElementById('author');
+    quoteAuthorContainer.innerHTML = ('<h6>' + "- "+ author + '</h6>');
+    // quoteAuthor.innerText = "- " + author;
 }
-const menuToggle = document.getElementById("menu-toggle");
-const navMenu = document.getElementById("nav-menu");
-
-menuToggle.addEventListener("click", () => {
-  navMenu.classList.toggle("hidden");
-});
